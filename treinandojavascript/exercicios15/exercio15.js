c7aadf0350ae3f73fb569d1fdddae63618d941cb
wellerson-pinheiros <wellersonpinheiros@outlook.com>
2024-04-03 20:17:46 -0300

    atualizei algumas coisas
    tentando fazer um sistema de soma baseado em logica e estrutura de repetiçao

---
+++ treinandojavascript/exercicios15/exercio15.js
@@ -0,0 +1,30 @@
+function calcular(){
+    let ini =  window.document.getElementById('verificar').value
+    let final = window.document.getElementById('fim').value
+    let  passo = window.document.getElementById('passos').value
+    let res = window.document.getElementById('res')
+//crie tres objetos com entrada de um valor (não convertido para numeros) para poder fazer os calculos
+
+
+if  (ini.value.length == 0 || final.value.length == 0 || passo.value.length == 0){
+    res.innerHTML = ('Impossivel contar')
+}else { 
+        res.innerHTML= ('contando')
+        let i = Number(ini.value)
+        let f = Number(final.value)
+        let p = Number(passo.value)
+        if( p <= 0) {
+            window.alert('impossivel contar! considerando passo 1')
+            p = 1
+        }}
+        if( i < f ) {
+        // contagem crescente
+        for ( let c = i; c <= f;c += p ){
+            res.innerHTML=(`${c} u\{1f449} `)
+        }} else {
+            for( let c = i; c >= f; c -= p) {
+                res.innerHTML=(`${c} u\{1f449}`) 
+            }}
+            
+
+}
